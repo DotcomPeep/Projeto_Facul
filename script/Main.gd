c476f69d823798be06b69e7323435755b995daef
DotcomPeep <steamcs2016@gmail.com>
2024-09-05 16:55:13 -0300

    First Commit

---
+++ script/Main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var started = false
+
+func _ready():
+	pass
+
+
---
+++ script/boss_character.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var life = 300
+var speed = 250
+var mov = Vector2()
+var damage = 25
+var speed_shoot = 450
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
